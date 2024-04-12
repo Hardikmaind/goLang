9cd65ecefb08abea2e781607f4a1eb4d15b3f7e8
Hardikmaind <maindhardik@gmail.com>
2024-04-12 21:23:48 +0530

    pushed the code

--- 22bitmorejson/main.go
+++ 22bitmorejson/main.go
@@ -67,10 +67,36 @@ func DecodeJson() {
 
 	var myOnlineData map[string]interface{}
 	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
-	fmt.Printf("%#v\n", myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)			//in order to print the map data we need to use the %#v. here we are print the interface data(map[string]interface{})
 
 	for k, v := range myOnlineData {
 		fmt.Printf("Key is %v  and value is %v and Type is: %T\n", k, v, v)
 	}
 
+fmt.Println("==========================================================================")
+
+	var hm=make(map[string]string)	
+	hm["name"]="saurabh"
+	hm["age"]="22"
+	hm["city"]="delhi"
+	hm["country"]="india"
+	fmt.Printf("this is the mapped data=>>>>>> %#v \n  ",hm)		//This line prints the map hm2 using %#v, which is a verb specifically designed to provide a Go-syntax representation of the value. For a map with string keys and integer values (map[string]int), %#v prints the map literal in a format that can be directly used as Go code to recreate the map.
+
+	fmt.Printf("this is the mapped data=>>>>>> %v \n  ",hm)	//This line prints the map hm using %v, which is a general-purpose verb that prints the default format for each type. For a map with string keys and string values (map[string]string), %v prints the key-value pairs in a readable format, similar to how you would define a map literal in Go code.
+
+
+     fmt.Println("=============================this is the map of type string and key and =============================================")
+
+
+
+
+
+	hm2 := map[string]int{
+        "apple":  5,
+        "banana": 3,
+        "cherry": 8,
+    }
+
+    // Print the map using %#v
+    fmt.Printf("Map: %#v\n", hm2)
 }
